560f16454bc4b6c8e41676ab6bac4aca5c9df1ae
Dom Vacchiano <djvacchiano@gmail.com>
2020-06-02 09:35:11 -0500

    adding initial project files

---
+++ for_loop_basic1.py
@@ -0,0 +1,33 @@
+#basic
+# for i in range (151):
+#     print(i)
+
+#Multiples of five
+# for i in range (5, 1001, 5):
+#     print(i)
+
+#Counting dojo way
+# for i in range (1, 101, 1):
+#     if i%10 == 0:
+#         print("Coding..")
+#     elif i%5 == 0:
+#         print("Coding Dojo..")
+
+# Woah suckers huge
+# sum = 0
+# for i in range (500001):
+#     if i%2 != 0:
+#         sum += i
+# print(sum)
+
+#countdown by fours
+# for i in range (2018, 0, -4):
+#     print(i)
+
+#Flexible counter
+# lowNum = 2
+# highNum = 9
+# mult = 3
+# for i in range (lowNum, highNum+1, 1):
+#     if (i % mult == 0):
+#         print(i)
