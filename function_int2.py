58cc809fba1a4961ca907b7bf4a74d6c9549501b
Dom Vacchiano <djvacchiano@gmail.com>
2020-06-03 09:02:47 -0500

    added function int 2 and did 1 of 4 for this assignment

---
+++ function_int2.py
@@ -0,0 +1,31 @@
+# iterate through lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+#1 change the value 10 in x to 15
+# print(x)
+# x[1][0] = 15
+# print(x)
+
+#2 change last name of the first student from Jordan to Bryant
+# print(students)
+# students[0]['last_name'] = 'Bryant'
+# print(students)
+
+#3 in the sports directory change Messi to Andres
+# print(sports_directory)
+# sports_directory['soccer'][0] = "Andres"
+# print(sports_directory)
+
+#4 Change value 20 in z to 30
+# print(z)
+# z[0]['y'] = 30
+# print(z)
