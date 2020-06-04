33081a75799d056eec5b37696bd578729b59144f
Dom Vacchiano <djvacchiano@gmail.com>
2020-06-04 09:30:34 -0500

    commented out function calls

--- function_int2.py
+++ function_int2.py
@@ -77,4 +77,4 @@ dojo = {
    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-printInfo(dojo)
+#printInfo(dojo)
